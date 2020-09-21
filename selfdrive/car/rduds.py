155bac51d0ba03c3ee147d11c542f59c5e675ef3
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-20 23:02:47 -0400

    add rd UDS.py

---
+++ selfdrive/car/rduds.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from tqdm import tqdm
+
+from panda.python.uds import NegativeResponseError
+
+from panda import Panda
+from panda.python.uds import UdsClient, SESSION_TYPE, CONTROL_TYPE, MESSAGE_TYPE
+
+ADDR=0x7d0
+
+if __name__ == "__main__":
+  panda = Panda()
+  panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+  uds_client = UdsClient(panda, ADDR, bus=0, timeout=1, debug=True)
+  uds_client.diagnostic_session_control(SESSION_TYPE.EXTENDED_DIAGNOSTIC)
+  uds_client.communication_control(CONTROL_TYPE.DISABLE_RX_DISABLE_TX, MESSAGE_TYPE.NORMAL)
+
+  print("querying addresses ...")
+  l = list(range(0x7d0))
+  with tqdm(total=len(l)) as t:
+    for i in l:
+      ct = i >> 8
+      mt = i & 0xFF
+      t.set_description(f"{hex(ct)} - {hex(mt)}")
+      try:
+        data = uds_client.diagnostic_session_control(ct, mt)
+        print(f"\n{ct} - {mt}: success")
+      except NegativeResponseError as e:
+        if e.message != "COMMUNICATION_CONTROL - sub-function not supported" and e.message != "COMMUNICATION_CONTROL - request out of range":
+          print(f"\n{ct} - {mt}: {e.message}")
+      t.update(1)
